75326493747a3ad8e3516b479c026f5df76414cf
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-09 12:09:59 -0400

    Add files via upload

---
+++ Introduction to random strings.py
@@ -0,0 +1,20 @@
+import math 
+dna_string='ACGATACAA'
+gc_content=[0.129,0.287,0.423, 0.476, 0.641, 0.742, 0.783]
+class gc_content_string:
+    def gc_content_string_(gc_content,string):
+        
+        list_of_calcs=[]
+        for gc in gc_content:
+            calc=0
+            for i in range(len(string)):
+                if string[i]=='A' or string[i]== 'T':
+                   calc+= math.log((1-gc)/2,10)
+               
+                elif string[i]=='C' or string[i]== 'G':
+                     calc+=math.log(gc/2,10)
+        
+            list_of_calcs.append(round(calc,3))
+        return print(list_of_calcs)
+if __name__ == "__main__":
+  gc_content_string.gc_content_string_(gc_content,dna_string)
