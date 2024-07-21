06f996bfecf6b29ec05b695263345c63446d4a33
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-21 17:37:44 -0400

    Update Introduction to random strings.py

--- Introduction to random strings.py
+++ Introduction to random strings.py
@@ -1,9 +1,5 @@
-import math 
-dna_string='ACGATACAA'
-gc_content=[0.129,0.287,0.423, 0.476, 0.641, 0.742, 0.783]
 class gc_content_string:
     def gc_content_string_(gc_content,string):
-        
         list_of_calcs=[]
         for gc in gc_content:
             calc=0
@@ -17,4 +13,7 @@ class gc_content_string:
             list_of_calcs.append(round(calc,3))
         return print(list_of_calcs)
 if __name__ == "__main__":
+  import math 
+  dna_string='ACGATACAA'
+  gc_content=[0.129,0.287,0.423, 0.476, 0.641, 0.742, 0.783]
   gc_content_string.gc_content_string_(gc_content,dna_string)
